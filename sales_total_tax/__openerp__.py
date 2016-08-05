d5b43db8b6983ab034c7c4f64ad101c0cb28260a
Shreejith Unnikrishnan <shreejith.u@eraminfotech.in>
2016-08-05 15:32:43 +0530

    Initial Commit

---
+++ sales_total_tax/__openerp__.py
@@ -0,0 +1,9 @@
+{
+    'name':'Sales Total Tax',
+    'description':'Apply Taxing  To the Sales Total Amount',
+    'author':'Odoo Ninja',
+    'depends':['base','sale'],
+    'data':['views/sale_view.xml'],
+    'application':True,
+    'installable':True
+}
