7cc379b674c1251edc8ab7160567cc49fde16b8f
Shreejith Unnikrishnan <shreejith.u@eraminfotech.in>
2016-08-05 15:37:54 +0530

    Initial Commit

--- sales_total_tax/__openerp__.py
+++ sales_total_tax/__openerp__.py
@@ -4,6 +4,8 @@
     'author':'Odoo Ninja',
     'depends':['base','sale'],
     'data':['views/sale_view.xml'],
+    'price':12.00,
+    'currency': 'EUR',
     'application':True,
     'installable':True
 }
