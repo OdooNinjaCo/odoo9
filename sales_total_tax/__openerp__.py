87910e8cd8e91faa9be40f179d260c19126010fe
Shreejith Unnikrishnan <shreejith.u@eraminfotech.in>
2016-08-05 18:27:12 +0530

    Initial Commit

--- sales_total_tax/__openerp__.py
+++ sales_total_tax/__openerp__.py
@@ -1,12 +1,21 @@
 {
     'name':'Sales Total Tax',
-    'description':'Apply Taxing  To the Sales Total Amount and Invoice Total Amount',
+    'description':"""
+    Avoids Order Line And Invoice Line Tax Calculations
+
+
+    Instaed applies Tax Calucation to the Final Sales Values and Invoice Value
+
+
+    Free Customization and Support will be provided  related to this work After the purchase of the module
+     """,
     'author':'Odoo Ninja',
     'depends':['base','sale','account'],
     'data':['views/sale_view.xml',
             'views/invoice_view.xml'],
     'price':30.00,
     'currency': 'EUR',
+    'email':'odooninja@gmail.com',
     'application':True,
     'installable':True
 }
